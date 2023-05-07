70a5c64767b0262e509b90c069eaf89625dc4ad7
jubamadu <jubamadu@ucsd.edu>
2023-05-06 19:55:38 -0700

    Added Image Changing

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,3 +5,12 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+
+  const selectElement = document.querySelector("#horn-select");
+  const myImage = document.querySelector('img[alt="No image selected"]');
+
+  selectElement.addEventListener("change", changeImage);
+
+  function changeImage(e){
+    myImage.src = `assets/images/${e.target.value}$.svg`;
+  }
