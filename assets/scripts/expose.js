9f796321bcadf5c415bddc336bca90330cb91249
jubamadu <jubamadu@ucsd.edu>
2023-05-06 20:04:40 -0700

    Completed First Image Change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -9,8 +9,16 @@ function init() {
   const selectElement = document.querySelector("#horn-select");
   const myImage = document.querySelector('img[alt="No image selected"]');
 
+  const volumeSlider = document.querySelector("#volume");
+  const audioVolume = document.querySelector(".hidden");
+
   selectElement.addEventListener("change", changeImage);
 
   function changeImage(e){
-    myImage.src = `assets/images/${e.target.value}$.svg`;
+    myImage.src = `./assets/images/${e.target.value}.svg`;
+  }
+
+
+  function changeVolumeImage(e){
+
   }
