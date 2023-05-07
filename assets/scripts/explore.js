5f5b1ae8baf67c6b3e04638cddb1e190b32e1598
jubamadu <jubamadu@ucsd.edu>
2023-05-06 21:30:13 -0700

    Added Text to Speech

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,10 +3,12 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  
+
   const synth = window.speechSynthesis;
   const voiceSelect = document.querySelector("#voice-select");
+  const text = document.querySelector("#text-to-speak");
   const playButton = document.getElementsByTagName('button');
+  const faceImage = document.querySelector("img[alt='Smiling face']")
   
   let voices = [];
 
@@ -32,12 +34,45 @@ function init() {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  playButton[0].addEventListener("click", myFunction);
+  playButton[0].addEventListener("click", speechValues);
+  
 
-  function myFunction() {
-    console.log("hello World");
+  function speechValues() {
+
+    const utterThis = new SpeechSynthesisUtterance(text.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    synth.speak(utterThis);
+    
+    flip(synth.speak);
+
+    function flip(e){
+      if(e){
+        faceImage.src = "./assets/images/smiling-open.png"
+      }
+      else{
+        faceImage.src = "./assets/images/smiling.png"
+      }
+    }
+    
+
+    }
+    
+    
   }
   
-}
+
+  
+    
+  
+  
+  
+
 
  
