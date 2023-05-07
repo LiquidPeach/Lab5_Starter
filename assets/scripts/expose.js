425a3ad3507f1e4cbd8f7016074fc0eb29903703
Simone <gaminggyrrl@gmail.com>
2023-05-06 20:53:08 -0700

    added okay sound function

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,19 +6,23 @@ function init() {
   // TODO
 }
 
+
   const selectElement = document.querySelector("#horn-select");
   const myImage = document.querySelector('img[alt="No image selected"]');
 
   const volumeSlider = document.querySelector("#volume");
   const audioVolume = document.querySelector(".hidden");
-
+  
   selectElement.addEventListener("change", changeImage);
 
   function changeImage(e){
     myImage.src = `./assets/images/${e.target.value}.svg`;
   }
-
-
+  
   function changeVolumeImage(e){
+    
+  }
 
+  function playSound(e) {
+    console.log("honk");
   }
